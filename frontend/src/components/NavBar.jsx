94409fa087bbd7377de55de0bb489b0604026f0d
Mbicha <mbithicharlse@gmail.com>
2024-06-23 14:53:07 +0300

    Changed livestock image in banner

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,13 +7,20 @@ const NavBar = () => {
             <header
                 className="relative flex flex-row h-11/12 w-screen">
                     <Link to={`/`}>
-                        <img src="/media/icons/menu-64.png" alt="" className="flex flex-col absolute top-3 left-3 z-10"/>
+                        <img src="/media/icons/menu-64.png" alt="" className="flex flex-col absolute top-3 left-3 z-50"/>
                     </Link>
                     <img src="/media/banners/maize.jpeg" alt="" className="w-1/2"/>
                     <img 
-                        src="/media/banners/livestock.jpeg" alt="" 
+                        src="/media/banners/cows.jpeg" alt="" 
                         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-72 w-72 z-10 border border-r border-green-100 p-1 rounded-full"/>
                     <img src="/media/banners/beefarming.jpeg" alt="" className="w-1/2"/>
+                    <div
+                        className="absolute flex flex-col text-white top-1/2 left-48 w-1/4 transform -translate-x-1/2 -translate-y-1/2 z-20">
+                            <h1 className="flex text-yellow-400 font-semibold text-xl">
+                                MyFarm
+                            </h1>
+                            Manage my farm produces, monitor profits, visualize metrices as low as $. 30 per month.
+                    </div>
             </header>
         </>
     )
