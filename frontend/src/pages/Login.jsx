e3ab5c08e393cb1ccc359e79314ea5755c87aa88
Mbicha <mbithicharlse@gmail.com>
2024-06-27 12:05:40 +0300

    Changed aesthetics in Login and Signup pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,8 +5,8 @@ const Login = () => {
     return(
         <>
             <section className="flex flex-col items-center bg-green-800 h-full w-screen">
-                <form className="flex flex-col p-3 rounded-lg bg-white w-auto">
-                    <h1 className="flex justify-center text-2xl rounded-tl-md rounded-tr-md items-center bg-yellow-500 border-b-green-800 font-bold w-full">
+                <form className="flex flex-col p-3 rounded-lg mt-2 bg-white w-auto">
+                    <h1 className="flex justify-center text-yellow-500 text-2xl rounded-tl-md rounded-tr-md items-center border-b border-green-800 font-bold w-full">
                         Login
                     </h1>
                     <input
@@ -25,7 +25,7 @@ const Login = () => {
                         <input type="submit" value="Login" className="mt-2 w-1/2 rounded-md bg-green-800 text-white p-1"/>
                     </div>
                     
-                    <p className="flex justify-center">No Account? <Link to={`/signup`} className="underline pl-2 pr-1 hover:bg-yellow-500">Signup</Link></p>
+                    <p className="flex justify-center mt-1">No Account? <Link to={`/signup`} className="underline pl-2 pr-1 hover:bg-yellow-500">Signup</Link></p>
                 </form>
             </section>
         </>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,9 +5,9 @@ const Signup = () => {
     return(
         <>
             <section className="flex flex-col items-center bg-green-800 h-full w-screen">
-                <form className="flex flex-col p-3 bg-white w-auto rounded-lg">
+                <form className="flex flex-col p-3 mt-2 bg-white w-auto rounded-lg">
                     <h1
-                        className="flex justify-center text-2xl items-center rounded-tl-md rounded-tr-md bg-yellow-500 border-b-green-800 font-bold w-full">
+                        className="flex justify-center text-yellow-500 text-2xl items-center rounded-tl-md rounded-tr-md border-b border-green-800 font-bold w-full">
                         Create Account
                     </h1>
                     <input
@@ -43,7 +43,7 @@ const Signup = () => {
                     <div className="flex justify-center">
                         <input type="submit" value="Signup" className="mt-2 w-1/2 rounded-md bg-green-800 text-white p-1"/>
                     </div>
-                    <p className="flex justify-center">Already have an Account? <Link to={`/login`} className="underline pl-1 pr-1 hover:bg-yellow-500">Login</Link></p>
+                    <p className="flex justify-center mt-1">Already have an Account? <Link to={`/login`} className="underline pl-1 pr-1 hover:bg-yellow-500">Login</Link></p>
                 </form>
             </section>
         </>
