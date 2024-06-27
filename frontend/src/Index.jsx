48c946cffdd5cd3313dcb47b9bcd354f4140852b
Mbicha <mbithicharlse@gmail.com>
2024-06-27 11:58:21 +0300

    Made landing page responsive

--- frontend/src/Index.jsx
+++ frontend/src/Index.jsx
@@ -7,16 +7,16 @@ const Index = () =>{
             <section
                 className="flex flex-col bg-green-800 h-full w-screen">
                     <div className="relative h-auto w-full">
-                        <img src="/media/banners/banner.svg" alt="" className="sm:h-1/2 md:h-2/3 lg:h-full w-full"/>               
+                        <img src="/media/banners/banner.svg" alt="" className="h-screen w-full"/>               
                         <div
-                            className="absolute flex flex-col text-white top-8 left-4 w-1/2 z-20">
+                            className="absolute flex flex-col text-white top-8 left-4 w-full z-20 sm:w-1/2">
                                 <h1 className="flex text-yellow-500 font-semibold text-2xl">
                                     MyFarm
                                 </h1>
                                 Manage my farm produces, monitor profits, visualize metrices with a pcket friendly budget of $. 30.00 per month.
-                                <div className="flex flex-row pt-2">
-                                    <Link to={`/create-farm`} className="bg-yellow-500 p-2 rounded-md text-green-800 mr-2">Create Farm</Link>
-                                    <Link to={`/signup`} className="bg-yellow-500 p-2 rounded-md text-green-800">Login/Signup</Link>
+                                <div className="flex flex-row pt-1">
+                                    <Link to={`/create-farm`} className="flex justify-center mt-1 bg-yellow-500 p-2 rounded-md text-green-800 mr-2">Create Farm</Link>
+                                    <Link to={`/signup`} className="flex justify-center mt-1 bg-yellow-500 p-2 rounded-md text-green-800">Login/Signup</Link>
                                 </div>                                
                         </div>
                     </div>
